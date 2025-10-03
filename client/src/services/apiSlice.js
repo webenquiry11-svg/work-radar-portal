5c29a46761e35541eff0e6af260b409834d51c70
starpublicity053 <ayushsharma.starpublicity@gmail.com>
2025-10-03 10:08:17 +0530

    feat: Add vercel.json to handle SPA routing

--- client/src/services/apiSlice.js
+++ client/src/services/apiSlice.js
@@ -2,7 +2,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
  
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://rmss-cd6o.onrender.com/api',
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
     if (token) {
